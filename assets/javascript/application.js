9d9472ba8a0889db5082330daeaa0212177cb79d
montekaka <josh.chen.jia@gmail.com>
2016-06-05 16:07:31 -0700

    add an animation to illusrate how the k means clusters work

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -7,8 +7,7 @@ var margin = {top: 20, right: 15, bottom: 60, left: 60}
 							.attr('height', height + margin.top + margin.bottom)
 							.style('background', '#C9D7D6')
 							.append('g')
-							.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+							.attr("transform", "translate(" + margin.left + "," + margin.top + ")");						
 		//setup x
 		var xValue = function(d){return d[0];},
 				xScale = d3.scale.linear().range([0, width]),
@@ -20,10 +19,12 @@ var margin = {top: 20, right: 15, bottom: 60, left: 60}
 				yMap   = function(d) { return yScale(yValue(d));},
 		    yAxis  = d3.svg.axis().scale(yScale).orient("left");
 
+		var colors = ['#FFB832', '#C61C6F', '#d21C82'];
+
 d3.json("http://localhost:4567/readings", function(error, data){
 	dataset = data['dataset'];	 
 	answers = data['answers'];
-	console.log(answers[0]['means']);
+	
 	// setup scales
 	xScale.domain([d3.min(dataset, xValue)-1, d3.max(dataset, xValue)+1]);
 	yScale.domain([d3.min(dataset, yValue)-1, d3.max(dataset, yValue)+1]);
@@ -63,10 +64,59 @@ d3.json("http://localhost:4567/readings", function(error, data){
   	 .delay(500)	   
  	   .attr("cx", xMap)
      .attr("cy", yMap);
-	
-  
 
-     
+  // How to select multiple selectors with selectAll?
+	// svg.selectAll(".class1, circle, #id2")
+
+	answers.forEach(function(m, i){
+		means = m['means'];
+		if (i == 0){
+			svg.selectAll(".means")
+				 .data(means)
+				 .enter().append('circle')
+				 .attr('class', "means")
+				 .attr("r",0)
+				 .transition()			 
+				 .attr("r",10)
+				 .style('fill','#C61C6F')
+				 .duration(1500)
+				 .delay(1500)
+				 .attr('cx', xMap)
+				 .attr('cy', yMap); 			
+		}else{
+			means.forEach(function(k,j){
+				plot_means = svg.selectAll(".means");			
+				d3.select(plot_means[0][j])
+					 .transition()
+					 .duration(1500)
+					 .delay(1500+i*1500)
+					 .style('fill','#C61C6F')
+					 .attr('cx', xMap(k))
+					 .attr('cy', yMap(k));
+			});
+		}
+	});
+
+  //svg.selectAll(".mean")
+  //	 .data(answers, function(d){  	 	
+  //	 	d['means']
+  //	 })
+  	 // .attr('class','p')
+  	 // .attr('class', "mean")
+  	 // .enter().append("circle")
+  	 // .attr("class", "mean")
+  	 // .attr("r",0)
+	   // .transition()
+	   // .attr("r",10)
+  	 // .duration(1500)
+  	 // .delay(1500);
+	   // .attr('cx', xMap)
+	   // .attr('cy', yMap);  	 
+  	 // .attr('cx', function(d){
+  	 // 	return xMap(d['means']);
+  	 // });
+
+  /*
 	svg.selectAll(".means")
 	   .data(answers.pop()['means'])
 	   .enter().append("circle")
@@ -78,6 +128,6 @@ d3.json("http://localhost:4567/readings", function(error, data){
   	 .delay(1500)	   
 	   .attr('cx', xMap)
 	   .attr('cy', yMap);
-	   
+	*/   
 
 });
